0349e83e2e596dcc9a58667c967c8c4bd24a8aaa
matias <matiasrozas.lp@gmail.com>
2023-11-27 00:30:35 -0300

    app

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { Dimensions, View } from 'react-native';
 import Register from './components/Register/Register';
+import { useEffect, useState } from 'react';
 export default function App() {
+
+  const [w, setW] = useState(Dimensions.get('window').width);
+
+  useEffect(() => {
+    const handleDimensionsChange = () => {
+      setW(Dimensions.get('window').width);
+    };
+
+    // Suscribirse al evento de cambio de dimensiones
+    Dimensions.addEventListener('change', handleDimensionsChange);
+
+    // Limpiar el suscriptor en el desmontaje del componente
+    return () => {
+      Dimensions.removeEventListener('change', handleDimensionsChange);
+    };
+  }, []);
+
   return (
     <View>
-      <Register></Register>
+      <Register/>
       <StatusBar style="auto" />
     </View>
   );
